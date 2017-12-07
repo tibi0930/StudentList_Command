283fbc54e36d166b6bd002cf5fbfeb20d0bdfe76
tibiatya <tibi0930@gmail.com>
2017-12-07 19:59:30 +0100

    Not perfectly work

---
+++ StudentList_Command/ViewModel/StudentAddCommand.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace StudentList_Command.ViewModel
+{
+    public class StudentAddCommand : ICommand
+    {
+        private StudentsViewModel _viewModel;
+
+        public StudentAddCommand(StudentsViewModel viewModel)
+        {
+            _viewModel = viewModel;
+        }
+
+        public event EventHandler CanExecuteChanged;
+
+        public Boolean CanExecute(Object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            _viewModel.AddNewStudent();
+        }
+    }
+}
--- StudentList_Command/ViewModel/StudentsViewModel.cs
+++ StudentList_Command/ViewModel/StudentsViewModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 
 namespace StudentList_Command.ViewModel
@@ -8,16 +9,38 @@ namespace StudentList_Command.ViewModel
     {
         public ObservableCollection<Student> Students { get; set; }
 
+        public Student newStudent { get; private set; }
+
+        public StudentAddCommand AddCommand { get; private set; }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
         public StudentsViewModel()
         {
-            Students = new ObservableCollection<Student>
-            {
+            Students = new ObservableCollection<Student>();
+            newStudent = new Student();
+            AddCommand = new StudentAddCommand(this); // parancs létrehozása
 
-                //Students.Add(new Student { Id = 1, LastName = "Kis", FirstName = "János", StudentCode = "KIJSAAI" });
-                new Student { Id = 2, LastName = "Nagy", FirstName = "Ferenc", StudentCode = "NAFSAAI" },
-                new Student { Id = 3, LastName = "Huba", FirstName = "Hugó", StudentCode = "HUHSAAI" },
-                new Student { Id = 4, LastName = "Gem", FirstName = "Géza", StudentCode = "GEGSAAI" }
-            };
+            Students.Add(new Student { Id = 1, LastName = "Kis", FirstName = "János", StudentCode = "KIJSAAI" });
+            Students.Add(new Student { Id = 2, LastName = "Nagy", FirstName = "Ferenc", StudentCode = "NAFSAAI" });
+            Students.Add(new Student { Id = 3, LastName = "Huba", FirstName = "Hugó", StudentCode = "HUHSAAI" });
+            Students.Add(new Student { Id = 4, LastName = "Gem", FirstName = "Géza", StudentCode = "GEGSAAI" });
+            // a tulajdonságokat objektuminicializálás segítségével hozzuk létre
+        }
+
+        public void AddNewStudent()
+        {
+            Students.Add(newStudent);
+            newStudent = new Student();
+            OnPropertyChanged("newStudent");
+        }
+
+        private void OnPropertyChanged(string property)
+        {
+            if(PropertyChanged != null)
+            {
+                PropertyChanged(this, new PropertyChangedEventArgs(property));
+            }
         }
     }
 }
